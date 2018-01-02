8a2d79a3131cf0b4cf086110765b062c0f305742
jacobdparker <jacobdparker@gmail.com>
2018-01-02 14:21:41 -0700

    simple test was not doing anything.  Dspk is failing to identify spikes in simple test.

--- src/python/dspk/dspk.py
+++ src/python/dspk/dspk.py
@@ -6,68 +6,75 @@ import tensorflow as tf
 
 def dspk(data):
 
-    #initialize a Tesor Flow Session
-    with tf.Session():
+    #build Tensor Flow Graph
 
 
-        # contruct 3-D averaging kernel and form Tensor of shape (filter_depth, filter_height, filter_width, in_channel, out_channel)
-        mu_kernel = np.ones([5,5,5])
-        mu_kernel = np.expand_dims(mu_kernel,-1)
-        mu_kernel = np.expand_dims(mu_kernel,-1)
+    # contruct 3-D averaging kernel and form Tensor of shape (filter_depth, filter_height, filter_width, in_channel, out_channel)
+    mu_kernel = np.ones([5,5,5])
+    mu_kernel = np.expand_dims(mu_kernel,-1)
+    mu_kernel = np.expand_dims(mu_kernel,-1)
 
-        mu_krn = tf.convert_to_tensor(mu_kernel, dtype =np.float32)
+    mu_krn = tf.convert_to_tensor(mu_kernel, dtype =np.float32)
 
-        #form data cube into tensor of shape (batch, depth, height, width, channels)
-        data = np.expand_dims(data,0)
-        data = np.expand_dims(data,-1)
+    #form data cube into tensor of shape (batch, depth, height, width, channels)
+    data = np.expand_dims(data,0)
+    data = np.expand_dims(data,-1)
 
-        dt= tf.convert_to_tensor(data,dtype=np.float32)
+    dt= tf.convert_to_tensor(data,dtype=np.float32)
 
 
 
-        #form a map of good and bad pixels witht the same dimensions as data and form Tensor
-        goodmap = data*0+1
-        gm = tf.convert_to_tensor(goodmap,dtype=np.float32)
+    #form a map of good and bad pixels witht the same dimensions as data and form Tensor
+    goodmap = data*0+1
+    gm = tf.convert_to_tensor(goodmap,dtype=np.float32)
 
-        #initilize bad pixel count to zero
-        bad_num = tf.constant(0,dtype=tf.float32)
-        new_bad = tf.constant(0,dtype=tf.float32)
+    #initilize bad pixel count to zero
+    bad_num = tf.constant(0,dtype=tf.float32)
+    new_bad = tf.constant(0,dtype=tf.float32)
 
-        #Function to identify bad pixels
-        def identify_bad_pix(gm,mu_krn,dt,bad_num,new_bad):
-            #Calculate running normilization
-            norm = tf.nn.conv3d(gm,mu_krn,strides=[1,1,1,1,1],padding = "SAME")
+    #Function to identify bad pixels
+    def identify_bad_pix(gm,mu_krn,dt,bad_num,new_bad):
+        #Calculate running normilization
+        norm = tf.nn.conv3d(gm,mu_krn,strides=[1,1,1,1,1],padding = "SAME")
 
-            #Calculate a neighborhood mean
-            g_data = tf.multiply(gm,dt)
-            n_mean = tf.nn.conv3d(g_data,mu_krn,strides=[1,1,1,1,1],padding = "SAME")
-            n_mean = tf.divide(n_mean,norm)
+        #Calculate a neighborhood mean
+        g_data = tf.multiply(gm,dt)
+        n_mean = tf.nn.conv3d(g_data,mu_krn,strides=[1,1,1,1,1],padding = "SAME")
+        n_mean = tf.divide(n_mean,norm)
 
-            #Deviation
-            dev = tf.subtract(g_data,n_mean)
-            n_std = tf.nn.conv3d(tf.square(dev),mu_krn,strides=[1,1,1,1,1],padding="SAME")
-            n_std = tf.divide(n_std,norm)
+        #Deviation
+        dev = tf.subtract(g_data,n_mean)
+        n_std = tf.nn.conv3d(tf.square(dev),mu_krn,strides=[1,1,1,1,1],padding="SAME")
+        n_std = tf.divide(n_std,norm)
 
-            #Compare pixel deviation to local standard deviation
-            sigmas = tf.constant(4.5)
+        #Compare pixel deviation to local standard deviation
+        sigmas = tf.constant(4.5)
 
-            bad = tf.greater_equal(dev,tf.multiply(sigmas,n_std))
-            bad = tf.cast(bad,tf.float32)
-            new_bad = tf.subtract(tf.reduce_sum(bad),bad_num)
+        bad = tf.greater_equal(dev,tf.multiply(sigmas,n_std))
+        bad = tf.cast(bad,tf.float32)
+        new_bad = tf.subtract(tf.reduce_sum(bad),bad_num)
 
-            #update good map and count bad pixels
-            gm = tf.subtract(gm,bad)
-            bad_num = tf.add(bad_num,new_bad)
 
-            return (gm,mu_krn,dt,bad_num,new_bad)
+        #update good map and count bad pixels
+        gm = tf.subtract(gm,bad)
+        bad_num = tf.add(bad_num,new_bad)
 
-        #setup while loop convergence condition
-        def end_bad_pix_search(gm,mu_krn,dt,bad_num,new_bad):
-            return tf.less_equal(tf.divide(new_bad,bad_num),.01)
+        return (gm,mu_krn,dt,bad_num,new_bad)
 
-        tf.while_loop(end_bad_pix_search,identify_bad_pix,(gm,mu_krn,dt,bad_num,new_bad))
-        return gm
-    return gm
+    #setup while loop convergence condition
+    def end_bad_pix_search(gm,mu_krn,dt,bad_num,new_bad):
+        return tf.less_equal(tf.divide(new_bad,bad_num),.01)
+
+    find_bad_pix = tf.while_loop(end_bad_pix_search,identify_bad_pix,(gm,mu_krn,dt,bad_num,new_bad))
+
+    sess = tf.Session()
+    list = sess.run(find_bad_pix)
+    sess.close()
+
+
+
+
+    return list
 
 
     # define a near-local smoothing kernel for replacing bad pixels
--- src/python/dspk/simple_test.py
+++ src/python/dspk/simple_test.py
@@ -2,15 +2,17 @@ import numpy as np
 from dspk import dspk
 
 
-spikes = np.ones([64,1024,1024])
+spikes = np.ones([100,1024,1024])
 
-spikes[10,10,10] = 10
-spikes[10,10,20] = 10
-spikes[10,10,30] = 10
-spikes[10,10,43] = 10
+spikes[10,10,10] = 100
+spikes[10,10,20] = 100
+spikes[10,10,30] = 100
+spikes[10,10,43] = 100
 
 gm = dspk(spikes)
-print(np.where(spikes > 1))
+gm = np.squeeze(gm)
+print(gm.shape)
+print([gm[3],gm[4]])
 
 
 
