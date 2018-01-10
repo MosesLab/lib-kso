37312a93c2e6021d9b872d52f02d889eb3c8a5c8
byrdie <roytsmart@gmail.com>
2018-01-10 00:53:55 -0700

    Testing IDL-Python bridge

--- src/python/dspk/dspk_util.py
+++ src/python/dspk/dspk_util.py
@@ -1,12 +1,40 @@
 import numpy as np
 
-def flatten_cube(orig_data, sz_x, sz_y, sz_z):
+def flatten_cube(data_3D, sz_x, sz_y, sz_z):
 
     panes = int(np.sqrt(sz_z))
-    orig_data_flat = np.reshape(orig_data, [sz_x, sz_y, panes, panes])
+    orig_data_flat = np.reshape(data_3D, [sz_x, sz_y, panes, panes])
     orig_data_flat = np.swapaxes(orig_data_flat, 1, 2)
     orig_data_flat = np.swapaxes(orig_data_flat, 0, 1)
     orig_data_flat = np.swapaxes(orig_data_flat, 2, 3)
-    orig_data_flat = np.reshape(orig_data_flat, [sz_x * panes, sz_y * panes])
+    data_3D_flat = np.reshape(orig_data_flat, [sz_x * panes, sz_y * panes])
 
-    return orig_data_flat
+    return data_3D_flat
+
+def add_frame(data_3D, axes, f_sz=1):
+
+    f = f_sz
+    sz = np.shape(data_3D)
+
+    i = 0
+    for s in sz:    # iterate through shape since we don't know how big it is
+        t = s - 1
+        if i in axes:
+
+            data = np.rollaxis(data_3D, i)   # access arbitrary dimension of the array
+
+            print(data.shape)
+
+            # Apply frame
+            data[0:f,:,:] = 0
+            data[s-f:s,:,:] = 0
+
+
+            data_3D = np.rollaxis(data, np.mod(-i,2))  # undo roll
+
+            print(data.shape)
+
+        i = i + 1   # keep track of our own looping variable
+
+
+    return data_3D
--- src/python/dspk/random_test.py
+++ src/python/dspk/random_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from dspk_util import flatten_cube
+import dspk_util
 
 from dspk import dspk
 
 sz_x = 32
 sz_y = 32
-sz_z = np.square(2)  # must be a square!!
+sz_z = np.square(3)  # must be a square!!
 
 slice_z = 0
 
@@ -17,7 +17,8 @@ noise_mean = 64
 spike_mean = 512
 
 plt_dev = 2
-plt_min = noise_mean - plt_dev * np.sqrt(noise_mean)
+plt_min = 0
+# plt_min = noise_mean - plt_dev * np.sqrt(noise_mean)
 plt_max = spike_mean + plt_dev * np.sqrt(spike_mean)
 
 rand = np.random.RandomState(seed=None)
@@ -26,10 +27,8 @@ rand = np.random.RandomState(seed=None)
 orig_data = rand.poisson(lam=64, size=[sz_x,sz_y,sz_z])
 
 # Put frames around data for easier viewing
-# orig_data[0,:,:] = 0
-# orig_data[sz_x -1,:,:] = 0
-# orig_data[:,0,:] = 0
-# orig_data[:,sz_y - 1,:] = 0
+frame = 1
+orig_data = dspk_util.add_frame(orig_data, [0, 1], f_sz=frame)
 
 # Add random spikes
 spike_mask = np.zeros([sz_x,sz_y,sz_z], dtype=np.int32)
@@ -39,9 +38,9 @@ for i in range(n_spk):
     while True:
 
         # Select random coordinate
-        x = rand.randint(0, sz_x, 1)
-        y = rand.randint(0, sz_y, 1)
-        z = rand.randint(0, sz_z, 1)
+        x = rand.randint(frame, sz_x-frame, 1)
+        y = rand.randint(frame, sz_y-frame, 1)
+        z = rand.randint(frame, sz_z-frame, 1)
 
         # Check if coordinate has been selected before
         if spike_mask[x,y,z] == 0:
@@ -60,18 +59,21 @@ for i in range(n_spk):
 (dspk_data,good_map,bad_pix_number) = dspk(orig_data, std_dev=2.0)
 
 # Flatten cube so we can view as image
-orig_data_flat = flatten_cube(orig_data, sz_x, sz_y, sz_z)
-good_map_flat = flatten_cube(good_map, sz_x, sz_y, sz_z)
-dspk_data_flat = flatten_cube(dspk_data, sz_x, sz_y, sz_z)
+orig_data_flat = dspk_util.flatten_cube(orig_data, sz_x, sz_y, sz_z)
+good_map_flat = dspk_util.flatten_cube(good_map, sz_x, sz_y, sz_z)
+dspk_data_flat = dspk_util.flatten_cube(dspk_data, sz_x, sz_y, sz_z)
 
 f1 = plt.figure()
 plt.imshow(orig_data_flat,vmin=plt_min, vmax=plt_max)
 
-f2 = plt.figure()
-plt.imshow(good_map_flat)
+# f2 = plt.figure()
+# plt.imshow(good_map_flat)
 
 f3 = plt.figure()
 plt.imshow(dspk_data_flat, vmin=plt_min, vmax=plt_max)
+
+# f3 = plt.figure()
+# plt.imshow(orig_data[:,:,slice_z], vmin=plt_min, vmax=plt_max)
 
 
 
