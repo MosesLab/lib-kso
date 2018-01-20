89383ecd01fdf6f161f7a37f480e343f0a6b0d35
Roy Smart <roytsmart@gmail.com>
2018-01-20 12:57:54 -0700

    Started C++ project with custom makefiles

--- src/kso/cpp/img/dspk/dspk.cpp
+++ src/kso/cpp/img/dspk/dspk.cpp
@@ -1,12 +1,10 @@
 
-
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "dspk.h"
 
 
 void kso::img::dspk::remove_noise(){
+
+
 
 	printf("hello world\n");
 
@@ -17,3 +15,5 @@ BOOST_PYTHON_MODULE(dspk){
     boost::python::def("remove_noise", kso::img::dspk::remove_noise);
 
 }
+
+
--- src/kso/cpp/img/dspk/dspk.h
+++ src/kso/cpp/img/dspk/dspk.h
@@ -8,7 +8,15 @@
 #ifndef DSPK_H_
 #define DSPK_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
 
 namespace kso {
 
