b8dd9af349afa4f34ca8a9269af487b59fde0436
byrdie <roytsmart@gmail.com>
2018-02-02 17:02:30 -0700

    still trying to understand IRIS fits files

--- src/kso/cuda/instrument/IRIS/read_fits.cu
+++ src/kso/cuda/instrument/IRIS/read_fits.cu
@@ -11,19 +11,31 @@ using namespace CCfits;
 
 void read_fits_raster(std::string path, float * buf){
 
-	std::auto_ptr<FITS> pInfile(new FITS(path,Read,true));
+	std::auto_ptr<FITS> pInfile(new FITS(path,Read,false));
 
 	PHDU& image = pInfile->pHDU();
+
 
 	std::valarray<unsigned long>  contents;
 
 	// read all user-specifed, coordinate, and checksum keys in the image
 	image.readAllKeys();
-
+//
 	image.read(contents);
 
 	// this doesn't print the data, just header info.
 	std::cout << image << std::endl;
+
+
+	ExtHDU& ext = pInfile->extension(3);
+
+
+	ext.readAllKeys();
+
+	ext.read(contents);
+
+	std::cout << ext << std::endl;
+
 
 }
 
